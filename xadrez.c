ba00240ed50ecdf435de5aa6c6278321b2713ea9
filipedemolina <filipemolininha2006@hotmail.com>
2025-04-22 17:46:49 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,60 @@
 #include <stdio.h>
-
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+//estrutura void da torre
+void torrem(int torre2){
+    if (torre2>0) 
+    {
+        printf("esquerda\n");
+        torrem(torre2-1);
+    }
+    }
+//estrutura void do bispo
+void bispom(int bispo2){
+    if (bispo2>0)
+    {
+        printf("Cima\n");
+        for (int i = 0; i < 1; i++)
+        {
+            printf("direita\n");
+        }
+        bispom(bispo2-1);
+    }
+}  
+//estrutura void da rainha
+void rainham(int rainha2){
+if (rainha2>0)
+{
+    printf("esquerda\n");
+    rainham(rainha2-1);
 }
+}
+//estrutura void do cavalo
+void cavalom(int cavalo2){
+    if (cavalo2>0){
+    
+    for (int i = 0; i < 2; i++)
+        {
+            printf("Cima\n");
+        }
+            printf("direita\n");
+        cavalom(cavalo2-1);
+    }
+    }
+    
+int main(){
+    //variáveis para armazenar a quantidade de casas que ira andar
+     int bispo=5;
+     int torre =5;
+     int rainha=8;  
+     int cavalo=1;
+     printf("torre ira 5 vezes para a esquerda\n");
+    torrem(torre);
+    printf ("bispo ira 5 vezes na diagonal\n");
+    bispom(bispo);
+    printf("rainha ira 8 vezes para esquerda\n");
+    rainham(rainha);
+    printf("o cavalo ira se movimentar em L\n");
+    cavalom(cavalo);
+
+return 0;
+}
+
